b029b74d68bddea39d41959bff495ef2c2aa6e3d
Kampfkarren <boynedmaster@gmail.com>
2019-04-04 19:09:01 -0700

    Docs, Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,13 @@ pub enum Error<'a> {
     TokenizerError(tokenizer::TokenizerError),
 }
 
+/// Creates an [Ast](ast/struct.Ast.html) from Lua code
 pub fn parse(code: &str) -> Result<ast::Ast, Error> {
     let tokens = tokenizer::tokens(code).map_err(Error::TokenizerError)?;
     ast::Ast::from_tokens(tokens).map_err(Error::AstError)
 }
 
+/// Prints back Lua code from an [Ast](ast/struct.Ast.html)
 pub fn print<'a>(ast: &'a ast::Ast<'a>) -> String {
     ast.tokens
         .iter()
