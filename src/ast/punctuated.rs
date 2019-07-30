f6c0b7ffb6a1ae7cb261d0f59c9a3750dc76628a
Kampfkarren <boynedmaster@gmail.com>
2019-07-30 15:07:01 -0700

    Documentation

--- src/ast/punctuated.rs
+++ src/ast/punctuated.rs
@@ -23,7 +23,9 @@ use crate::{
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq)]
+/// A punctuated sequence of node `T` separated by [`TokenReference`](../tokenizer/enum.TokenReference.html).
+/// Refer to the [module documentation](index.html) for more details.
+#[derive(Clone, Debug, Default, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct Punctuated<'a, T> {
     #[cfg_attr(feature = "serde", serde(borrow))]
@@ -31,44 +33,54 @@ pub struct Punctuated<'a, T> {
 }
 
 impl<'a, T> Punctuated<'a, T> {
+    /// Creates an empty punctuated sequence
     pub fn new() -> Self {
         Self {
             pairs: Vec::new(),
         }
     }
 
+    /// Returns whether there's any nodes in the punctuated sequence
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
+    /// Returns the number of pairs in the punctuated sequence
     pub fn len(&self) -> usize {
         self.pairs.len()
     }
 
+    /// Returns an iterator over the [`Pair`](enum.Pair.html) sequences
     pub fn into_pairs(self) -> impl Iterator<Item = Pair<'a, T>> {
         self.pairs.into_iter()
     }
 
+    /// Returns an iterator over references of the sequence values, ignoring punctuation
     pub fn iter(&self) -> Iter<'a, '_, T> {
         self.into_iter()
     }
 
+    /// Returns an iterator over mutable references of the sequence values, ignoring punctuation
     pub fn iter_mut(&'a mut self) -> impl Iterator<Item = &'a mut T> {
         self.pairs.iter_mut().map(Pair::value_mut)
     }
 
+    /// Returns an iterator over the [`Pair`](enum.Pair.html) sequences as references
     pub fn pairs(&self) -> impl Iterator<Item = &Pair<'a, T>> {
         self.pairs.iter()
     }
 
+    /// Returns an iterator over the [`Pair`](enum.Pair.html) sequences as mutable references
     pub fn pairs_mut(&mut self) -> impl Iterator<Item = &mut Pair<'a, T>> {
         self.pairs.iter_mut()
     }
 
+    /// Pops off the last [`Pair`](enum.Pair.html), if it isn't empty
     pub fn pop(&mut self) -> Option<Pair<'a, T>> {
         self.pairs.pop()
     }
 
+    /// Pushes a new [`Pair`](enum.Pair.html) onto the sequence
     pub fn push(&mut self, pair: Pair<'a, T>) {
         self.pairs.push(pair);
     }
@@ -137,6 +149,8 @@ impl<'a: 'b, 'b, T> IntoIterator for &'b mut Punctuated<'a, T> {
     }
 }
 
+/// An iterator over owned values of type `T`.
+/// Refer to the [module documentation](index.html) for more details.
 pub struct IntoIter<'a, T> {
     inner: std::vec::IntoIter<Pair<'a, T>>,
 }
@@ -149,6 +163,8 @@ impl<'a, T> Iterator for IntoIter<'a, T> {
     }
 }
 
+/// An iterator over borrowed values of type `&T`.
+/// Refer to the [module documentation](index.html) for more details.
 pub struct Iter<'a, 'b, T> {
     inner: std::slice::Iter<'b, Pair<'a, T>>,
 }
@@ -161,6 +177,8 @@ impl<'a, 'b, T> Iterator for Iter<'a, 'b, T> {
     }
 }
 
+/// An iterator over borrowed values of type `&mut T`.
+/// Refer to the [module documentation](index.html) for more details.
 pub struct IterMut<'a, 'b, T> {
     inner: std::slice::IterMut<'b, Pair<'a, T>>,
 }
@@ -173,10 +191,15 @@ impl<'a, 'b, T> Iterator for IterMut<'a, 'b, T> {
     }
 }
 
+/// A node `T` followed by the possible trailing [`TokenReference`](../tokenizer/enum.TokenReference.html).
+/// Refer to the [module documentation](index.html) for more details.
 #[derive(Clone, Debug, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub enum Pair<'a, T> {
+    /// A node `T` with no trailing punctuation
     End(T),
+
+    /// A node `T` followed by punctuation (in the form of a [`TokenReference`](../tokenizer/enum.TokenReference.html))
     Punctuated(
         T,
         #[cfg_attr(feature = "serde", serde(borrow))] TokenReference<'a>,
@@ -184,6 +207,7 @@ pub enum Pair<'a, T> {
 }
 
 impl<'a, T> Pair<'a, T> {
+    /// Creates a `Pair` with node `T` and optional punctuation
     pub fn new(value: T, punctuation: Option<TokenReference<'a>>) -> Self {
         match punctuation {
             None => Pair::End(value),
@@ -191,6 +215,7 @@ impl<'a, T> Pair<'a, T> {
         }
     }
 
+    /// Takes the `Pair` and returns the node `T` and the punctuation, if it exists as a tuple
     pub fn into_tuple(self) -> (T, Option<TokenReference<'a>>) {
         match self {
             Pair::End(value) => (value, None),
@@ -198,13 +223,12 @@ impl<'a, T> Pair<'a, T> {
         }
     }
 
+    /// Takes the `Pair` and returns the node `T`
     pub fn into_value(self) -> T {
-        match self {
-            Pair::End(value) => value,
-            Pair::Punctuated(value, _) => value,
-        }
+        self.into_tuple().0
     }
 
+    /// Returns a reference to the node `T`
     pub fn value(&self) -> &T {
         match self {
             Pair::End(value) => value,
@@ -212,6 +236,7 @@ impl<'a, T> Pair<'a, T> {
         }
     }
 
+    /// Returns a mutable reference to the node `T`
     pub fn value_mut(&mut self) -> &mut T {
         match self {
             Pair::End(value) => value,
@@ -219,6 +244,7 @@ impl<'a, T> Pair<'a, T> {
         }
     }
 
+    /// Returns the trailing punctuation, if it exists
     pub fn punctuation(&self) -> Option<&TokenReference<'a>> {
         match self {
             Pair::End(_) => None,
