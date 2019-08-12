1791eb25611189e2b93cba9af0e81d12836e06db
Kampfkarren <boynedmaster@gmail.com>
2019-08-12 02:22:49 -0700

    Documentation, tokens_mut

--- src/ast/span.rs
+++ src/ast/span.rs
@@ -1,12 +1,23 @@
+//! A representation of a "contained span", or a span within specific bounds.
+//!
+//! Examples of contained spans include:
+//! - Arguments in a function call use parentheses `(...)`
+//! - Indexing a table uses brackets `[...]`
+//! - Creating a table uses braces `{...}`
+//!
+//! Contained spans don't contain the inner data, just the start and end bounds.
+//! ```
 use crate::{
     node::Node,
     private::Sealed,
-    tokenizer::{Position, Token, TokenReference},
+    tokenizer::{Position, TokenReference},
 };
 
 use full_moon_derive::Visit;
 use serde::{Deserialize, Serialize};
 
+/// A contained span with the beginning and ending bounds.
+/// Refer to the [module documentation](index.html) for more details.
 #[derive(Clone, Debug, PartialEq, Visit)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub struct ContainedSpan<'a> {
@@ -16,6 +27,7 @@ pub struct ContainedSpan<'a> {
 }
 
 impl<'a> ContainedSpan<'a> {
+    /// Creates a contained span from the start and end bounds
     pub fn new(
         start: TokenReference<'a>,
         end: TokenReference<'a>,
@@ -23,8 +35,14 @@ impl<'a> ContainedSpan<'a> {
         Self { tokens: (start, end) }
     }
 
-    pub fn tokens(&self) -> (&Token<'a>, &Token<'a>) {
+    /// Returns the start and end bounds in a tuple as references
+    pub fn tokens(&self) -> (&TokenReference<'a>, &TokenReference<'a>) {
         (&self.tokens.0, &self.tokens.1)
+    }
+
+    /// Returns the start and end bounds in a tuple as mutable references
+    pub fn tokens_mut(&mut self) -> (&mut TokenReference<'a>, &mut TokenReference<'a>) {
+        (&mut self.tokens.0, &mut self.tokens.1)
     }
 }
 
