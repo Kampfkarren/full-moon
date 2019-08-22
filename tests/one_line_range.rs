b1deb4a9d16b0400a0b864e0bfc46525412b84ae
Kampfkarren <boynedmaster@gmail.com>
2019-08-21 22:36:09 -0700

    Add test to ensure one line ranges

---
+++ tests/one_line_range.rs
@@ -0,0 +1,23 @@
+use full_moon::{parse, node::Node};
+
+#[test]
+fn test_one_line_range() {
+	let ast = parse(r#"
+
+	local x = 1
+
+	local y = 1
+
+	local function x() print(1) end
+
+	function x() print(1) end
+
+	for index, value in pairs(list) do print(index, value) end
+
+	"#).unwrap();
+
+	for stmt in ast.nodes().iter_stmts() {
+		let (start, end) = stmt.range().unwrap();
+		assert_eq!(end.line() - start.line(), 0, "node {:?} does not have a range on the same line", stmt);
+	}
+}
