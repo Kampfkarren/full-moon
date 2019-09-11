9b2b0706dd85898cec88563afadb3fafb3f92cb1
Kampfkarren <boynedmaster@gmail.com>
2019-09-11 15:08:37 -0700

    0.4.0-rc.2

---
+++ tests/comments_around_functions.rs
@@ -0,0 +1,66 @@
+use full_moon::{
+    self,
+    ast::*,
+    visitors::Visitor,
+    node::Node,
+    tokenizer::TokenKind,
+    tokenizer::Token,
+};
+use std::error::Error;
+use owned::Owned;
+
+const CODE: &str = r#"
+
+local Module = {}
+Module.__index = Module
+
+--[[
+    Creates a new instance of Module.
+
+    ```lua
+        Module.new()
+    ```
+
+    @static
+    @param name string - This is the name for this Module.
+    @returns Module - Returns the new Module!
+]]
+function Module.new(name)
+
+end
+
+"#;
+
+struct MemberVisitor<'a> {
+    comments: Vec<String>,
+    ast: &'a Ast<'static>,
+}
+
+impl Visitor<'static> for MemberVisitor<'_> {
+    fn visit_function_declaration(&mut self, function: &FunctionDeclaration<'static>) {
+        if let Some((tokens, _)) = function.surrounding_ignore_tokens(&self.ast) {
+            let mut tokens = tokens.clone();
+            tokens.retain(|&t| t.token_kind() == TokenKind::MultiLineComment);
+            self.comments.extend(tokens.into_iter().map(|t| t.to_string()).collect::<Vec<String>>())
+        }
+    }
+}
+
+fn generate() -> Result<(), Box<dyn Error>> {
+    let ast = full_moon::parse(CODE)?.owned();
+
+    let mut visitor = MemberVisitor {
+        comments: Vec::new(),
+        ast: &ast,
+    };
+
+    visitor.visit_ast(&ast);
+
+    println!("{:?}", visitor.comments);
+
+    Ok(())
+}
+
+fn main() {
+    generate().expect("Oh");
+}
