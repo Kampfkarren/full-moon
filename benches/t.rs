60a93d7d0ed8c0b63e5de366513e5ec601f08918
Kampfkarren <boynedmaster@gmail.com>
2019-10-10 21:46:15 -0700

    Split benchmarks

--- benches/t.rs
+++ benches/t.rs
@@ -2,16 +2,24 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 const T_SOURCE: &str = include_str!("./t.lua");
 
-fn criterion_benchmark(criterion: &mut Criterion) {
-    criterion.bench_function("parse t", |b| {
-        b.iter(|| full_moon::parse(black_box(T_SOURCE)))
+fn tokenize(criterion: &mut Criterion) {
+    criterion.bench_function("tokenize t", |b| {
+        b.iter(|| full_moon::tokenizer::tokens(black_box(T_SOURCE)))
+    });
+}
+
+fn parse(criterion: &mut Criterion) {
+    let tokens = full_moon::tokenizer::tokens(T_SOURCE).unwrap();
+
+    criterion.bench_function("get ast from parsed t", move |b| {
+        b.iter(|| full_moon::ast::Ast::from_tokens(black_box(tokens.clone())))
     });
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(20);
-    targets = criterion_benchmark
+    targets = tokenize, parse
 }
 
 criterion_main!(benches);
