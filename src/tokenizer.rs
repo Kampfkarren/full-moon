ac34e6063110c63cb0807423dd232fc520c1ab4b
boyned//Kampfkarren <boynedmaster@gmail.com>
2019-10-27 22:33:39 -0700

    Fix #41 (#46)

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -579,7 +579,7 @@ impl<'a> fmt::Display for StringLiteralQuoteType {
 lazy_static! {
     static ref PATTERN_IDENTIFIER: Regex = Regex::new(r"[^\W\d]+\w*").unwrap();
     static ref PATTERN_NUMBER: Regex =
-        Regex::new(r"^((-?0x[A-Fa-f\d]+)|(-?((\d*\.\d+)|(\d+))([eE]-?\d+)?))").unwrap();
+        Regex::new(r"^((0x[A-Fa-f\d]+)|(((\d*\.\d+)|(\d+))([eE]-?\d+)?))").unwrap();
     static ref PATTERN_COMMENT_MULTI_LINE_BEGIN: Regex = Regex::new(r"--\[(=*)\[").unwrap();
     static ref PATTERN_COMMENT_SINGLE_LINE: Regex = Regex::new(r"--([^\n]*)").unwrap();
     static ref PATTERN_STRING_MULTI_LINE_BEGIN: Regex = Regex::new(r"\[(=*)\[").unwrap();
