fcefa3c37225339c74a8b357fe622ca14ade432f
Kampfkarren <boynedmaster@gmail.com>
2020-05-05 04:54:05 -0700

    Tabs -> spaces

--- full-moon/src/rlua.rs
+++ full-moon/src/rlua.rs
@@ -7,22 +7,22 @@ use rlua::ToLua;
 
 macro_rules! enum_userdata {
     ($object:path, {
-		$(
-			$tag: path $(=> $modify:ident)?,
-		)+
-	}) => {
+        $(
+            $tag: path $(=> $modify:ident)?,
+        )+
+    }) => {
         impl<'lua> rlua::ToLua<'lua> for $object {
             fn to_lua(self, lua: rlua::Context<'lua>) -> rlua::Result<rlua::Value<'lua>> {
                 match self {
-					$(
-						$tag(data) => {
-							$(
-								let data = $modify(lua, data)?;
-							)?
+                    $(
+                        $tag(data) => {
+                            $(
+                                let data = $modify(lua, data)?;
+                            )?
 
-							data.to_owned().to_lua(lua)
-						}
-					)+
+                            data.to_owned().to_lua(lua)
+                        }
+                    )+
                 }
             }
         }
@@ -31,54 +31,54 @@ macro_rules! enum_userdata {
 
 macro_rules! userdata {
     ($object:path, {
-		$(
-			properties: {
-				$($property_name:ident: $property_getter:expr,)+
-			},
-		)?
-	}) => {
+        $(
+            properties: {
+                $($property_name:ident: $property_getter:expr,)+
+            },
+        )?
+    }) => {
         impl rlua::UserData for $object {
             fn add_methods<'lua, M: rlua::UserDataMethods<'lua, Self>>(methods: &mut M) {
-				methods.add_meta_method(
-					rlua::MetaMethod::Index,
-					|lua, this, key: String| -> rlua::Result<rlua::Value<'lua>> {
-						match key.as_str() {
-							$(
-								$(
-									stringify!($property_name) => {
-										return $property_getter(lua, this)?.to_lua(lua);
-									},
-								)+
-							)?
+                methods.add_meta_method(
+                    rlua::MetaMethod::Index,
+                    |lua, this, key: String| -> rlua::Result<rlua::Value<'lua>> {
+                        match key.as_str() {
+                            $(
+                                $(
+                                    stringify!($property_name) => {
+                                        return $property_getter(lua, this)?.to_lua(lua);
+                                    },
+                                )+
+                            )?
 
-							key => {
-								return Err(rlua::Error::external(format!("{} is not a valid property", key)));
-							}
-						}
-					},
-				);
+                            key => {
+                                return Err(rlua::Error::external(format!("{} is not a valid property", key)));
+                            }
+                        }
+                    },
+                );
 
-				methods.add_meta_function(
-					rlua::MetaMethod::ToString,
-					|_, _: ()| -> rlua::Result<String> {
-						Ok(stringify!($object)
-							.replace("<'static>", "")
-							.to_owned()
-						)
-					}
-				);
+                methods.add_meta_function(
+                    rlua::MetaMethod::ToString,
+                    |_, _: ()| -> rlua::Result<String> {
+                        Ok(stringify!($object)
+                            .replace("<'static>", "")
+                            .to_owned()
+                        )
+                    }
+                );
 
-				methods.add_method("print", |_, this, _: ()| {
-					Ok(this.print())
-				});
+                methods.add_method("print", |_, this, _: ()| {
+                    Ok(this.print())
+                });
 
-				methods.add_method("visit", |_, this, mut visitor: LuaVisitor| {
-					// TODO: error handling
-					LuaVisit::visit(this, &mut visitor);
-					Ok(())
-				});
-			}
-		}
+                methods.add_method("visit", |_, this, mut visitor: LuaVisitor| {
+                    // TODO: error handling
+                    LuaVisit::visit(this, &mut visitor);
+                    Ok(())
+                });
+            }
+        }
     };
 }
 
@@ -196,101 +196,101 @@ impl<'lua> LuaVisit<'lua> for Ast<'static> {
 
 macro_rules! create_lua_visitor {
     ({
-		$(
-			$full_name:ident: $visitor_name:ident,
-		)+
-	}) => {
+        $(
+            $full_name:ident: $visitor_name:ident,
+        )+
+    }) => {
         #[derive(Default)]
         struct LuaVisitor<'lua> {
-			$(
-				$visitor_name: Option<rlua::Function<'lua>>,
-			)+
-		}
+            $(
+                $visitor_name: Option<rlua::Function<'lua>>,
+            )+
+        }
 
         impl<'lua> rlua::FromLua<'lua> for LuaVisitor<'lua> {
-			fn from_lua(value: rlua::Value<'lua>, _: rlua::Context<'lua>)
-				-> rlua::Result<Self>
-			{
-				if let rlua::Value::Table(table) = value {
-					let mut this = Self::default();
+            fn from_lua(value: rlua::Value<'lua>, _: rlua::Context<'lua>)
+                -> rlua::Result<Self>
+            {
+                if let rlua::Value::Table(table) = value {
+                    let mut this = Self::default();
 
-					for pair in table.pairs::<String, rlua::Function>() {
-						let (key, value) = pair?;
-						$(
-							match key.as_str() {
-								stringify!($full_name) => {
-									this.$visitor_name = Some(value);
-								}
+                    for pair in table.pairs::<String, rlua::Function>() {
+                        let (key, value) = pair?;
+                        $(
+                            match key.as_str() {
+                                stringify!($full_name) => {
+                                    this.$visitor_name = Some(value);
+                                }
 
-								invalid => {
-									return Err(rlua::Error::external(
-										format!("unknown node type '{}'", invalid),
-									));
-								}
-							}
-						)+
-					}
+                                invalid => {
+                                    return Err(rlua::Error::external(
+                                        format!("unknown node type '{}'", invalid),
+                                    ));
+                                }
+                            }
+                        )+
+                    }
 
-					Ok(this)
-				} else {
-					Err(rlua::Error::external("expected table of nodes to visit"))
-				}
-			}
-		}
+                    Ok(this)
+                } else {
+                    Err(rlua::Error::external("expected table of nodes to visit"))
+                }
+            }
+        }
 
         impl Visitor<'static> for LuaVisitor<'_> {
-			$(
-				fn $visitor_name(&mut self, node: &$full_name<'static>) {
-					if let Some(visitor) = &self.$visitor_name {
-						// TODO: error handling
-						visitor.call::<_, ()>(node.to_owned()).ok();
-					}
-				}
-			)+
-		}
+            $(
+                fn $visitor_name(&mut self, node: &$full_name<'static>) {
+                    if let Some(visitor) = &self.$visitor_name {
+                        // TODO: error handling
+                        visitor.call::<_, ()>(node.to_owned()).ok();
+                    }
+                }
+            )+
+        }
     };
 }
 
 create_lua_visitor!({
-	Return: visit_return,
+    Return: visit_return,
 });
 
 userdata!(Ast<'static>, {
     properties: {
-    	nodes: borrowed_return(Ast::nodes),
+        nodes: borrowed_return(Ast::nodes),
     },
 });
 
 userdata!(Block<'static>, {
     properties: {
-    	lastStmt: optional(Block::last_stmt),
+        lastStmt: optional(Block::last_stmt),
     },
 });
 
 userdata!(Return<'static>, {
     properties: {
-    	token: borrowed_return(Return::token),
+        token: borrowed_return(Return::token),
     },
 });
 
 userdata!(Token<'static>, {
-	properties: {
-		end_position: owned_return(Token::end_position),
-		start_position: owned_return(Token::start_position),
-	},
+    properties: {
+        end_position: owned_return(Token::end_position),
+        start_position: owned_return(Token::start_position),
+    },
 });
 
 userdata!(TokenReference<'static>, {
-	properties: {
-		leading_trivia: collect_iterator(TokenReference::leading_trivia),
-		token: borrowed_return(TokenReference::token),
-		trailing_trivia: collect_iterator(TokenReference::trailing_trivia),
-	},
+    properties: {
+        leading_trivia: collect_iterator(TokenReference::leading_trivia),
+        token: borrowed_return(TokenReference::token),
+        trailing_trivia: collect_iterator(TokenReference::trailing_trivia),
+    },
 });
 
 enum_userdata!(LastStmt<'static>, {
-	LastStmt::Break => decow_token_reference,
-	LastStmt::Return,
+    LastStmt::Break => decow_token_reference,
+    LastStmt::Return,
 });
 
 pub struct LuaApi;
