e26c59bed3b11bba9f8d9b833fc7e98677433f58
JohnnyMorganz <johnnymorganz@outlook.com>
2021-04-20 17:24:49 +0100

    Add fail cases test

--- full-moon/tests/fail_cases.rs
+++ full-moon/tests/fail_cases.rs
@@ -41,6 +41,27 @@ fn test_tokenizer_fail_cases() {
 }
 
 #[test]
+#[cfg(feature = "roblox")]
+#[cfg_attr(feature = "no-source-tests", ignore)]
+fn test_roblox_parser_fail_cases() {
+    run_test_folder("./tests/roblox_cases/fail/parser", |path| {
+        let source = fs::read_to_string(path.join("source.lua")).expect("couldn't read source.lua");
+
+        let tokens = tokenizer::tokens(&source).expect("couldn't tokenize");
+
+        assert_yaml_snapshot!("tokens", tokens);
+
+        match ast::Ast::from_tokens(tokens) {
+            Ok(_) => panic!("fail case passed for {:?}", path),
+            Err(error) => {
+                println!("error {:#?}", error);
+                assert_yaml_snapshot!("error", error);
+            }
+        }
+    })
+}
+
+#[test]
 #[cfg(feature = "lua52")]
 #[cfg_attr(feature = "no-source-tests", ignore)]
 fn test_lua52_parser_fail_cases() {
