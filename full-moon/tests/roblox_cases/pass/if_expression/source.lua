3caf2c12299eb4949ef1f2141348f986f0673ad7
JohnnyMorganz <johnnymorganz@outlook.com>
2021-10-30 14:29:41 +0100

    Update pass cases

--- full-moon/tests/roblox_cases/pass/if_expression/source.lua
+++ full-moon/tests/roblox_cases/pass/if_expression/source.lua
@@ -1,3 +1,6 @@
 local x = if foo then foo.x else 5
 local y = (if x then x.indices else create()):update(if shouldUpdate then information else defaults)
-local y = (if bar then foo.y else 5) :: number
+local z = (if bar then foo.y else 5) :: number
+
+local a = if foo then foo.x elseif bar then bar.x else 5
+local b = if foo then if bar then bar else foo else 5
