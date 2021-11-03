848b725cebf759a9c405bc4d15e34ecbc2f8afad
JohnnyMorganz <johnnymorganz@outlook.com>
2021-11-03 11:22:14 +0000

    Add more test cases

--- full-moon/tests/roblox_cases/pass/if_expression/source.lua
+++ full-moon/tests/roblox_cases/pass/if_expression/source.lua
@@ -4,3 +4,8 @@ local z = (if bar then foo.y else 5) :: number
 
 local a = if foo then foo.x elseif bar then bar.x else 5
 local b = if foo then if bar then bar else foo else 5
+local c = if foo then (foo.x :: number) elseif bar then bar.x()() else 5
+local d = if foo then 5 else baz :: number
+
+if if foo then bar else baz then
+end
