83066862b3057500ebd55fc3af51dfb86acf60c9
JohnnyMorganz <johnnymorganz@outlook.com>
2022-12-07 17:15:48 +0000

    Add test case

---
+++ full-moon/tests/lua52_cases/pass/numbers/source.lua
@@ -0,0 +1,14 @@
+-- fractional hexadecimal
+local a = 0x0.1E
+
+-- binary exponent in hexadecimal
+local b = 0xA23p-4
+
+-- a mixture of both
+local c = 0X1.921FB54442D18P+1
+
+-- LuaJIT numbers (ULL/LL ending for both decimal and hexidecimal, or imaginary)
+-- This is in Lua 5.2 tests for simplicity
+local d = 42LL
+local e = 0x2aLL
+local f = 12.5i
